832d64c7db4b9c0b70bd71654a0dbba91f40752b
Simerdeep Singh <simerdeep.singh@synapse.com>
2020-08-04 09:23:39 +0530

    - Heroku configure

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const { geocode, forecast } = require('./Utils');
 const e = require('express');
 
 const app = express();
-
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname,'../public')));
 const viewsPath = path.join(__dirname,'../templates/views');
@@ -103,6 +103,6 @@ app.get('*',(req,res) => {
     res.send("404 Page")
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is started")
 });
